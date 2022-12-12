0a763c17238381f456077b4c272b3e8b3828bbc2
Babak <sirbabimo@gmail.com>
2022-12-12 14:13:54 +0330

    Initial commit

---
+++ ListViewLazyLoading/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Toolkit.Collections;
+using Microsoft.Toolkit.Uwp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ListViewLazyLoading
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            var collection = new IncrementalLoadingCollection<PeopleSource, Person>();
+            mylist.ItemsSource = collection;
+        }
+
+ 
+
+
+    }
+}
---
+++ ListViewLazyLoading/PeopleSource.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Toolkit.Collections;
+using Microsoft.Toolkit.Uwp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ListViewLazyLoading
+{
+
+    // Be sure to include the using at the top of the file:
+    //using Microsoft.Toolkit.Uwp;
+
+    public class Person
+    {
+        public string Name { get; set; } 
+    }
+
+    public class PeopleSource : IIncrementalSource<Person>
+    {
+        private readonly List<Person> people;
+
+        public PeopleSource()
+        { 
+            people = new List<Person>(); 
+        }         
+
+        int lasti=1 ;
+        public async Task<IEnumerable<Person>> GetPagedItemsAsync(int pageIndex, int pageSize, CancellationToken cancellationToken = default)
+        { 
+            var result = (from p in people select p).Skip(pageIndex * pageSize).Take(pageSize); 
+            for (int i = 1; i <= 50; i++)
+            {
+                var p = new Person { Name = "Person " + lasti++ };
+                people.Add(p); 
+            }
+            // Simulates a longer request...
+            await Task.Delay(1000);
+            //var count = result.Count();
+            return result;
+        }
+    }
+
+
+}
